74ea3550e5cb50be66e9222ff950240f83996dc5
Martin <martinvalchev2@gmail.com>
2018-03-13 02:08:51 +0200

    added

---
+++ Programming Fundamentals Extended/Data Types and Methods - Exercise/Resurrection/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resurrection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int body = int.Parse(Console.ReadLine());
+            decimal totalYears = 0;
+
+            for (int i = 0; i < body; i++)
+            {
+                int lenght = int.Parse(Console.ReadLine());
+                decimal width = decimal.Parse(Console.ReadLine());
+                decimal lenghtOf1Wing = decimal.Parse(Console.ReadLine());
+
+                var powResult = BigInteger.Pow(lenght, 2);
+
+                totalYears = (decimal) powResult * (width + 2 * lenghtOf1Wing);
+                Console.WriteLine(totalYears);
+            }
+
+        }
+    }
+}
+ 
