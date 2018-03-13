8e19435511eb16cf8c73871b080dfed380cb03b9
Martin <martinvalchev2@gmail.com>
2018-03-13 02:03:27 +0200

    added

---
+++ Programming Fundamentals Extended/Data Types and Methods - Exercise/Solution/Poke Mon/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poke_Mon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int power = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            sbyte exhaustion = sbyte.Parse(Console.ReadLine());
+            int counter = 0;
+            int currentPower = power;
+
+          while (currentPower >= distance)
+            {
+               if (currentPower  == power / 2 && power % 2 == 0 && exhaustion != 0)
+                {
+                    currentPower /= exhaustion;
+                }
+               if (currentPower < distance)
+                {
+                    break;
+                }
+
+                currentPower -= distance;
+                counter++;
+               
+            }
+            Console.WriteLine(currentPower);
+            Console.WriteLine(counter);
+          
+
+        }
+    }
+}
---
+++ Programming Fundamentals Extended/Data Types and Methods - Exercise/Solution/Wormtest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wormtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long lenghtMeters = long.Parse(Console.ReadLine())*100;
+            double widhtCentimetars = double.Parse(Console.ReadLine());
+
+            double reminder = lenghtMeters / widhtCentimetars;
+
+            if (lenghtMeters % reminder == 0 || widhtCentimetars == 0)
+            {
+                Console.WriteLine($"{lenghtMeters * widhtCentimetars:F2}");
+            }
+            else
+            {
+                Console.WriteLine($"{reminder * 100:F2}%");
+            }
+        }
+    }
+}
