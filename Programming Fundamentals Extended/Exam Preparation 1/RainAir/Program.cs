6f1b3f49d0100905af49cbbcbb82bd4ae2b840d0
Martin <martinvalchev2@gmail.com>
2018-04-08 02:57:18 +0300

    added

--- Programming Fundamentals Extended/Exam Preparation 1/RainAir/Program.cs
+++ Programming Fundamentals Extended/Exam Preparation 1/RainAir/Program.cs
@@ -10,6 +10,42 @@ namespace RainAir
     {
         static void Main(string[] args)
         {
+            Dictionary<string, List<int>> customers = new Dictionary<string, List<int>>();
+            string comand = Console.ReadLine();
+
+            while (comand != "I believe I can fly!")
+            {
+                string[] info = comand.Split(' ').ToArray();
+                if (info[1] != "=")
+                {
+                    string customer = info[0];
+                    List<int> flight = new List<int>();
+                    for (int i = 1; i <= info.Length-1; i++)
+                    {
+                        flight.Add(int.Parse(info[i]));
+                    }
+                    if (!customers.ContainsKey(customer))
+                    {
+                        customers.Add(customer, flight);
+                    }
+                    else
+                    {
+                        customers[customer].AddRange(flight);
+                    }
+                }
+                else
+                {
+                    string custemer1 = info[0];
+                    string custemer2 = info[2];
+                    customers[custemer1] = customers[custemer2].ToList();
+                }
+
+                comand = Console.ReadLine();
+            }
+            foreach (var pair in customers.OrderByDescending(x=>x.Value.Count).ThenBy(x=>x.Key))
+            {
+                Console.WriteLine($"#{pair.Key} ::: {string.Join(", ",pair.Value.OrderBy(x=>x))}");
+            }
         }
     }
 }
--- Programming Fundamentals Extended/Exam Preparation 1/Raincast/Program.cs
+++ Programming Fundamentals Extended/Exam Preparation 1/Raincast/Program.cs
@@ -14,21 +14,49 @@ namespace Raincast
             string typePattern = @"^Type: (Normal|Warning|Danger)$";
             string sourcePattern = @"^Source: ([a-zA-Z0-9]+)$";
             string forecastPattern = @"^Forecast: ([^!.,?]+)$";
-            string current = "";
+            string type = "";
+            string source = "";
+            string forecast = "";
+            bool typeReady = false;
+            bool sourceReady = false;
+            bool forecastReady = false;
             List<string> result = new List<string>();
             string comand = Console.ReadLine();
             while (comand != "Davai Emo")
             {
-                Match typeMatch = Regex.Match(comand, typePattern);
-                Match sourceMatch = Regex.Match(comand, sourcePattern);
-                Match forecastMarch = Regex.Match(comand, forecastPattern);
-
-                Console.WriteLine($"({typeMatch.Groups[1]}) {forecastMarch.Groups[1]} ~ {sourceMatch.Groups[1]}");
-               
-                
+               if (typeReady == false && Regex.IsMatch(comand, typePattern))
+                {
+                    Match t = Regex.Match(comand, typePattern);
+                    type = t.Groups[1].Value;
+                    typeReady = true;
+                }
+                if (sourceReady == false && typeReady == true && Regex.IsMatch(comand, sourcePattern))
+                {
+                    Match s = Regex.Match(comand, sourcePattern);
+                    source = s.Groups[1].Value;
+                    sourceReady = true;
+                }
+                if (forecastReady == false && sourceReady == true && typeReady == true && Regex.IsMatch(comand, forecastPattern))
+                {
+                    Match f = Regex.Match(comand, forecastPattern);
+                    forecast = f.Groups[1].Value;
+                    forecastReady = true;
+                }
+                if (forecastReady == true && sourceReady == true && typeReady == true)
+                {
+                    string final = $"({type}) {forecast} ~ {source}";
+                    result.Add(final);
+                    typeReady = false;
+                    sourceReady = false;
+                    forecastReady = false;
+                }
                 comand = Console.ReadLine();
             }
-           
+
+            foreach (var member in result)
+            {
+                Console.WriteLine(member);
+            }
 
         }
     }
