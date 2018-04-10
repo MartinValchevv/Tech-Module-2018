18712c4f8915060f7e71487398ac007b5fe9115a
Martin <martinvalchev2@gmail.com>
2018-04-10 14:51:15 +0300

    added

---
+++ Programming Fundamentals Extended/Exam - 20 August 2017/Entertrain/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entertrain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int power = int.Parse(Console.ReadLine());
+            int sum = 0;
+            List<int> train = new List<int>();
+            string input = Console.ReadLine();
+
+            while (input != "All ofboard!")
+            {
+                int wagon = int.Parse(input);
+                sum += wagon;
+                train.Add(wagon);
+                if (sum > power)
+                {
+                    int average = sum / train.Count;
+                    int first = train.OrderBy(x => Math.Abs(x - average)).First();
+                    train.Remove(first);
+                }
+                input = Console.ReadLine();
+            }
+            train.Reverse();
+            List<int> sorted = new List<int>(train) { power };
+            Console.WriteLine(String.Join(" ", sorted));
+        }
+    }
+}
---
+++ Programming Fundamentals Extended/Exam - 20 August 2017/Trainers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, decimal> teams = new Dictionary<string, decimal> { { "Technical Trainers", 0M}, { "Practical Trainers", 0M}, { "Theoretical Trainers", 0M } };
+
+            for (int i = 0; i < n; i++)
+            {
+                long milies = long.Parse(Console.ReadLine());
+                decimal tons = decimal.Parse(Console.ReadLine());
+                string team = Console.ReadLine();
+                long distanceMeters = milies * 1600;
+                decimal cargoKilograms = tons * 1000;
+                decimal money = (cargoKilograms * 1.5M) - ( 0.7M * distanceMeters * 2.5M);
+                
+                switch (team)
+                {
+                    case "Technical":
+                        teams["Technical Trainers"] += money;
+                        break;
+                    case "Practical":
+                        teams["Practical Trainers"] += money;
+                        break;
+                    case "Theoretical":
+                        teams["Theoretical Trainers"] += money;
+                        break;
+
+                }
+
+            }
+            var winner = teams.OrderByDescending(x => x.Value).FirstOrDefault();
+
+            Console.WriteLine($"The {winner.Key} win with ${winner.Value:F3}.");
+        }
+    }
+}
