60a51fd9a2a3cfbf38c56e353973b65ec2c839ce
Martin <martinvalchev2@gmail.com>
2018-04-15 00:06:36 +0300

    added

---
+++ Programming Fundamentals Extended/Exam - 04 Sept 2017/Phoenix Grid/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Phoenix_Grid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^([^\s_]{3}\.)+([^\s_]{3})*$";
+            string comand = Console.ReadLine();
+            while(comand != "ReadMe")
+            {
+                if (Regex.IsMatch(comand, pattern) || comand.Length == 3)
+                {
+                    bool isPalindrome = true;
+
+                    for (int i = 0; i < comand.Length / 2; i++)
+                    {
+                        if (comand[i] != comand[comand.Length - 1 - i])
+                        {
+                            Console.WriteLine("NO");
+                            isPalindrome = false;
+                            break;
+                        }
+                    }
+                    if (isPalindrome)
+                    {
+                        Console.WriteLine("YES");
+                    }
+                    
+                } 
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+              
+                comand = Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ Programming Fundamentals Extended/Exam Preparation 3/Charity Marathon/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Charity_Marathon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long days = long.Parse(Console.ReadLine());
+            long runners = long.Parse(Console.ReadLine());
+            long laps = long.Parse(Console.ReadLine());
+            long lapsLenght = long.Parse(Console.ReadLine());
+            long trackCapacity = long.Parse(Console.ReadLine());
+            double money = double.Parse(Console.ReadLine());
+       
+            long maxRunners = trackCapacity * days;
+            long willRun = Math.Min(maxRunners, runners);
+            long totalMeters = willRun * laps * lapsLenght;
+            long totalKilometars = totalMeters / 1000;
+            double moneyRaised = totalKilometars * money;
+
+            Console.WriteLine($"Money raised: {moneyRaised:F2}");
+
+        }
+    }
+}
